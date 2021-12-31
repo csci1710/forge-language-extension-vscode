29197c3b7cfaffd210d3b86b020f4835afcbd4c7
Emmie He <yimin_he@brown.edu>
2021-12-31 11:02:08 -0800

    clean up comments

--- server/src/server.ts
+++ server/src/server.ts
@@ -234,53 +234,8 @@ async function validateTextDocument(textDocument: TextDocument): Promise<void> {
 
 
 		});
-
-		// connection.console.log("end spawning");
-
-
+		// connection.console.log("end");
 	});
-
-	// // In this simple example we get the settings for every validate run.
-	// const settings = await getDocumentSettings(textDocument.uri);
-
-	// // The validator creates diagnostics for all uppercase words length 2 and more
-	// const text = textDocument.getText();
-	// const pattern = /\b[A-Z]{2,}\b/g;
-	// let m: RegExpExecArray | null;
-
-	// let problems = 0;
-	// const diagnostics: Diagnostic[] = [];
-	// while ((m = pattern.exec(text)) && problems < settings.maxNumberOfProblems) {
-	// 	problems++;
-	// 	const diagnostic: Diagnostic = {
-	// 		severity: DiagnosticSeverity.Warning,
-	// 		range: {
-	// 			start: textDocument.positionAt(m.index),
-	// 			end: textDocument.positionAt(m.index + m[0].length)
-	// 		},
-	// 		message: `${m[0]} is all uppercase.`,
-	// 		source: 'ex'
-	// 	};
-	// 	if (hasDiagnosticRelatedInformationCapability) {
-	// 		diagnostic.relatedInformation = [
-	// 			{
-	// 				location: {
-	// 					uri: textDocument.uri,
-	// 					range: Object.assign({}, diagnostic.range)
-	// 				},
-	// 				message: 'Spelling matters'
-	// 			},
-	// 			{
-	// 				location: {
-	// 					uri: textDocument.uri,
-	// 					range: Object.assign({}, diagnostic.range)
-	// 				},
-	// 				message: 'Particularly for names'
-	// 			}
-	// 		];
-	// 	}
-	// 	diagnostics.push(diagnostic);
-	// }
 }
 
 connection.onDidChangeWatchedFiles(_change => {
