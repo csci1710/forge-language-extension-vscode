309034929f462316bd0988d82f4e7bc7be4c585d
Emmie He <yimin_he@brown.edu>
2022-01-16 15:04:18 -0800

    added a `hacky` term link handler

--- client/src/extension.ts
+++ client/src/extension.ts
@@ -14,7 +14,7 @@ let client: LanguageClient;
 function getTerminal(fileURI: string): vscode.Terminal | null {
 	const terms = (<any>vscode.window).terminals;
 	const termLength = terms.length;
-	for (let i = 0; i < terms.length; i++){
+	for (let i = 0; i < terms.length; i++) {
 		if (terms[i].name === fileURI) {
 			return terms[i];
 		}
@@ -25,6 +25,56 @@ function getTerminal(fileURI: string): vscode.Terminal | null {
 export function activate(context: ExtensionContext) {
 	// console.log("Forge Client activated!");
 
+	// inspired by: https://github.com/GrandChris/TerminalRelativePath/blob/main/src/extension.ts
+	// todo: this seems to be overriding existing link provider?
+	vscode.window.registerTerminalLinkProvider({
+		provideTerminalLinks: (context, token) => {
+			const forgeFileReg = /[\\/]?([^\\/\n]*\.frg):(\d+):(\d+):?/;
+			const matcher = (context.line as string).match(forgeFileReg);
+			if (matcher === undefined) {
+				return [];
+			} else {
+				console.log(`matched forge file: ${matcher}`);
+
+				const filename = matcher[1];
+				// verify that filename matches?
+				const line = parseInt(matcher[2]);
+				const col = parseInt(matcher[3]);
+
+				const tooltip = vscode.window.activeTextEditor.document.uri.fsPath + `:${line}:${col}`;
+
+				return [
+					{
+						startIndex: matcher.index,
+						length: matcher[0].length,
+						tooltip: tooltip,
+						filePath: vscode.window.activeTextEditor.document.uri.fsPath,
+						line: line,
+						column: col
+					}
+				];
+			}
+		},
+		handleTerminalLink: (link: any) => {
+
+			if (link.line !== undefined) {
+				console.log(`link.line detected: ${link.filePath}`);
+				const start = new vscode.Position(link.line, link.column);
+				const end = new vscode.Position(link.line, link.column);
+				const range = new vscode.Range(start, end);
+
+				const opts: vscode.TextDocumentShowOptions = {
+					selection: range
+				};
+
+				vscode.commands.executeCommand("vscode.open", vscode.Uri.file(link.filePath), opts);
+			}
+			else {
+				vscode.commands.executeCommand('vscode.open', vscode.Uri.file(link.filePath));
+			}
+		}
+	});
+
 	// The command has been defined in the package.json file
 	// Now provide the implementation of the command with registerCommand
 	// The commandId parameter must match the command field in package.json
@@ -33,9 +83,11 @@ export function activate(context: ExtensionContext) {
 		console.log("runFile Command starts ...");
 		const filepath = vscode.window.activeTextEditor.document.uri.fsPath;
 		let terminal: vscode.Terminal | null = getTerminal(filepath);
-		if (!terminal){
+		if (!terminal) {
 			terminal = vscode.window.createTerminal(`${filepath}`);
 		}
+
+		// hack for forge error display
 
 		terminal.sendText(`clear`);
 		terminal.show();
