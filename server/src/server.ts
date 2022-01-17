27f3f3699f8c3e14dfaecdfde45b663dd17f9aee
Emmie He <yimin_he@brown.edu>
2022-01-16 16:37:49 -0800

    handler eof from racket side; adding timestamps to end sockets earlier - this is still pretty wasteful, should consider queuing up the sockets creation maybe

--- server/src/server.ts
+++ server/src/server.ts
@@ -144,12 +144,11 @@ function spawnRacket(): ChildProcess {
 		connection.console.error("Cannot launch racket");
 		throw new Error("Cannot launch racket");
 	}
-	if (racket.stderr) {
-		racket.stderr.on('data', (data: string) => {
-			connection.console.error(`Racket Err: ${data}`);
-		});
-		// throw new Error("Racket stopped");
-	}
+	// if (racket.stderr) {
+	// 	racket.stderr.on('data', (data: string) => {
+	// 		connection.console.log(`Racket data: ${data}`);
+	// 	});
+	// }
 	connection.console.log("Successfully spawned Racket");
 	return racket;
 }
@@ -165,104 +164,118 @@ documents.onDidChangeContent(change => {
 	validateTextDocument(change.document);
 });
 
+let timestamp = Date.now();
+
 async function validateTextDocument(textDocument: TextDocument): Promise<void> {
 	// const settings = await getDocumentSettings(textDocument.uri);
+	// when validate is called, we assign this validation a timestamp
+	const myTimestamp = Date.now();
+	timestamp = myTimestamp;
 
 	const text = textDocument.getText();
+	const buf = new Buffer(4);
+	buf.writeUInt32LE(text.length, 0);
+
 	const diagnostics: Diagnostic[] = [];
 
 	let myStderr = '\n';
 	connection.console.log(">>>>>>>>>> Connecting to socket");
 	const so = new net.Socket();
-    so.connect(8879, 'localhost');
-
-	so.on('data', function (data) {
-		myStderr += data;
-		connection.console.log(data.toString());
-		connection.console.log(">>>>>>>>>> Ending socket");
-		so.end();
-
-		// when racket finishes eval, we parse the received stderr and send diagnostics to client
-		if (myStderr !== '\n') {
-			let start = 0;
-			let end = 0;
-
-			const line_match = /line=(\d+)/.exec(myStderr);
-			const column_match = /column=(\d+)/.exec(myStderr);
-			// let offset_match = parser(myStderr, /offset=(\d+)/);
-
-			let line_num = 0;
-			let col_num = 0;
-
-			// the stderr could be tokenization issues
-			if (line_match !== null && column_match !== null) {
-				connection.console.log(`line match: ${line_match[0]}, col match: ${column_match[0]}`);
-				line_num = parseInt(line_match[0].slice('line='.length));
-				col_num = parseInt(column_match[0].slice('column='.length));
-
-			} else {
-				// for now this would not happen
-				const special_match = /frg:(\d+):(\d+):/.exec(myStderr);
-				if (special_match !== null) {
-					line_num = parseInt(special_match[1]);
-					col_num = parseInt(special_match[2]);
-				}
-			}
-
-			// if we get some line/col number, we want to get the start and end of the error
-			if (line_num !== 0) {
-				// connection.console.log(`line num: ${line_num}, col num: ${col_num}`);
-				let m: RegExpExecArray | null;
-				// iterate over each line
-				const pattern = /(.*)\n/g;
-				while (line_num > 0 && (m = pattern.exec(text))) {
-					// connection.console.log(`match: ${m[0]}, ${m.index}`);
-					start = m.index + col_num;
-					end = m.index + m[0].length;
-					line_num -= 1;
-				}
-				// connection.console.log(`start: ${start}, end: ${end}`);
-			}
-
-			const diagnostic: Diagnostic = {
-				severity: DiagnosticSeverity.Warning,
-				range: {
-					start: textDocument.positionAt(start),
-					end: textDocument.positionAt(end)
-				},
-				message: `Forge Syntax Error: ${myStderr}`,
-				source: 'syntax_check.rkt'
-			};
-			if (hasDiagnosticRelatedInformationCapability) {
-				diagnostic.relatedInformation = [
-					{
-						location: {
-							uri: textDocument.uri,
-							range: Object.assign({}, diagnostic.range)
-						},
-						message: `${myStderr}`
-					}
-				];
-			}
-			diagnostics.push(diagnostic);
-		}
-		// Send the computed diagnostics to VSCode.
-		connection.sendDiagnostics({ uri: textDocument.uri, diagnostics });
-	});
+	so.connect(8879, 'localhost');
 
 	so.on('error', function (error) { connection.console.log(`client received error: ${error.toString()}`); });
 	so.on('connect', function () {
 		connection.console.log("Connected to the socket");
-		// import {Buffer} from 'buffer'
-		const buf = new Buffer(4);
-		buf.writeUInt32LE(text.length, 0);
-		so.write(buf);
-		connection.console.log(`>>>>>>>>  text size is: ${text.length} ${buf}`);
-		so.write(text);
-		connection.console.log("write text to the racket server");
-		// so.end();
+		// it is possible that when we connect, there are already new validation being assigned
+		// in this case we want to quit to allow others to enter the socket
+		if (myTimestamp < timestamp) {
+			connection.console.log(`oops I connected too late: current timestamp is ${timestamp}, my timestamp is ${myTimestamp}`);
+			so.end();
+		} else {
+			so.write(buf);
+			connection.console.log(`>>>>>>>>  text size is: ${text.length} ${buf}`);
+			so.write(text);
+			connection.console.log("write text to the racket server");
+		}
 	});
+	so.on('data', function (data) {
+		if (myTimestamp < timestamp) {
+			connection.console.log(`oops my data arrived too late: current timestamp is ${timestamp}, my timestamp is ${myTimestamp}`);
+			so.end();
+		} else {
+			myStderr += data;
+			connection.console.log(data.toString());
+			connection.console.log(">>>>>>>>>> Ending socket");
+			so.end();
 
+			// when racket finishes eval, we parse the received stderr and send diagnostics to client
+			if (myStderr !== '\n') {
+				let start = 0;
+				let end = 0;
+
+				const line_match = /line=(\d+)/.exec(myStderr);
+				const column_match = /column=(\d+)/.exec(myStderr);
+				// let offset_match = parser(myStderr, /offset=(\d+)/);
+
+				let line_num = 0;
+				let col_num = 0;
+
+				// the stderr could be tokenization issues
+				if (line_match !== null && column_match !== null) {
+					connection.console.log(`line match: ${line_match[0]}, col match: ${column_match[0]}`);
+					line_num = parseInt(line_match[0].slice('line='.length));
+					col_num = parseInt(column_match[0].slice('column='.length));
+
+				} else {
+					// for now this would not happen
+					const special_match = /frg:(\d+):(\d+):/.exec(myStderr);
+					if (special_match !== null) {
+						line_num = parseInt(special_match[1]);
+						col_num = parseInt(special_match[2]);
+					}
+				}
+
+				// if we get some line/col number, we want to get the start and end of the error
+				if (line_num !== 0) {
+					// connection.console.log(`line num: ${line_num}, col num: ${col_num}`);
+					let m: RegExpExecArray | null;
+					// iterate over each line
+					const pattern = /(.*)\n/g;
+					while (line_num > 0 && (m = pattern.exec(text))) {
+						// connection.console.log(`match: ${m[0]}, ${m.index}`);
+						start = m.index + col_num;
+						end = m.index + m[0].length;
+						line_num -= 1;
+					}
+					// connection.console.log(`start: ${start}, end: ${end}`);
+				}
+
+				const diagnostic: Diagnostic = {
+					severity: DiagnosticSeverity.Warning,
+					range: {
+						start: textDocument.positionAt(start),
+						end: textDocument.positionAt(end)
+					},
+					message: `Forge Syntax Error: ${myStderr}`,
+					source: 'syntax_check.rkt'
+				};
+				if (hasDiagnosticRelatedInformationCapability) {
+					diagnostic.relatedInformation = [
+						{
+							location: {
+								uri: textDocument.uri,
+								range: Object.assign({}, diagnostic.range)
+							},
+							message: `${myStderr}`
+						}
+					];
+				}
+				diagnostics.push(diagnostic);
+			}
+			// Send the computed diagnostics to VSCode.
+			connection.sendDiagnostics({ uri: textDocument.uri, diagnostics });
+		}
+	});
 	// Send the computed diagnostics to VSCode.
 	connection.sendDiagnostics({ uri: textDocument.uri, diagnostics });
 
@@ -320,7 +333,7 @@ connection.onExit(() => {
 
 connection.onExecuteCommand(async (params) => {
 	connection.console.log("Detected command execution!");
-	if (params.command !== 'forge.runFile' || params.arguments ===  undefined) {
+	if (params.command !== 'forge.runFile' || params.arguments === undefined) {
 		return;
 	}
 	connection.console.log("Detected run execution!");
--- server/src/syntax_check.rkt
+++ server/src/syntax_check.rkt
@@ -19,18 +19,20 @@
 
 (define (process-content s-in s-out) 
   (with-handlers ([exn:fail? 
-                   (lambda (v) 
-				     (displayln (exn-message v) (current-error-port)) 
-					 (write (exn-message v) s-out))] 
-	              [exn:break:terminate? 
-				   (lambda (v) (tcp-close server) (exit))]) 
+                    (lambda (v) 
+				              (displayln (exn-message v) (current-error-port)) 
+					            (write (exn-message v) s-out))] 
+	                [exn:break:terminate? 
+				             (lambda (v) (tcp-close server) (exit))]) 
 
-	; the client should first send an 4-byte integer to indicate the string length 
-	(display "waiting for bytes\n")
-	(define count (integer-bytes->integer (read-bytes 4 s-in) #f #f))
-	(displayln count (current-error-port)) 
-    (display "got some bytes\n")
-	(my-check-syntax (read-string count s-in)))
+    ; the client should first send an 4-byte integer to indicate the string length 
+    ; (display "waiting for bytes\n")
+    
+    (define count (with-handlers ([exn:fail? (lambda (v) 0)]) (integer-bytes->integer (read-bytes 4 s-in) #f #f)))
+    ; (displayln count (current-error-port)) 
+    ; (display "got some bytes\n")
+    (my-check-syntax (read-string count s-in)))
+
    (close-output-port s-out)
   (start-accept))
 
