d599e7c0c764b090d0f453a91407cb81d0657e4e
Siddhartha Prasad <siddhartha.a.prasad@gmail.com>
2023-02-08 08:49:38 -0500

    Log forge version

--- client/src/logger.ts
+++ client/src/logger.ts
@@ -1,14 +1,28 @@
 import { initializeApp } from 'firebase/app';
 import { getFirestore, collection, doc, setDoc } from 'firebase/firestore/lite';
+const execSync = require('child_process').execSync;
+
+        // I would also like to get the Forge lang version here.
+
 
 import config from "./logging_config.json";
 
 export class Logger {
 
-    user; app; db; log_target;
+    user; app; db; log_target; version;
 
     constructor(userid: string)
     {
+        let v = null;
+        try
+        {
+            v = execSync('raco pkg show --full-checksum forge');
+        }
+        catch {}
+
+        this.version = (v != null && v.length > 0) ? 
+                            new TextDecoder().decode(v) : "unknown";
+
         this.user = userid;
         this.app = initializeApp(config);
         this.db = getFirestore(this.app)
