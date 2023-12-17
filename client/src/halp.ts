d32222e390ddac444b6cd1d2dd650e9c4de92e97
Siddhartha Prasad <siddhartha.a.prasad@gmail.com>
2023-12-17 11:23:50 +0530

    Beginning of HALP

--- client/src/halp.ts
+++ client/src/halp.ts
@@ -1,30 +1,44 @@
+import {RacketProcess} from './racketprocess';
+import * as vscode from 'vscode';
+import * as fs from 'fs';
 
-
-function runHalp(studentTests: string, testFileName : string): void {
+function runHalp(studentTests: string, testFileName: string): string {
 	
-
+	const forgeOutput = vscode.window.createOutputChannel('Forge Output');
+	const forgeEvalDiagnostics = vscode.languages.createDiagnosticCollection('Forge Eval');
+	let racket: RacketProcess = new RacketProcess(forgeEvalDiagnostics, forgeOutput);
 
 	// Get the appropriate includes
-	// const wheatText = getWheat(testFileName);
+	const wheatText = getWheat(testFileName);
 
-	// const toRun = studentTests + wheatText;
+	const toRun = studentTests + wheatText;
 
+	// Write the contents of toRun to a temporary file
+	const tempFilePath = tempFile();
+	try {
+		fs.writeFileSync(tempFilePath, toRun);
 
+		// Need to examine and interpret results here.
+		let r = racket.runFile(tempFilePath);
 
-
-	// // Run racket against the new file
-	// const output = await runRacket(tempFilePath);
-
-	// // Parse the output and display results in a side window
-	// const results = parseOutput(output);
-
-
-
-	// return results;
+		return "";
+	} finally {
+		// Delete the temporary file in the finally block
+		fs.unlinkSync(tempFilePath);
+	}
 }
+function tempFile(): string {
+	const characters = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789';
+	const length = 10;
+	let result = '';
 
+	for (let i = 0; i < length; i++) {
+		const randomIndex = Math.floor(Math.random() * characters.length);
+		result += characters.charAt(randomIndex);
+	}
 
-
+	return result + '.rkt';
+}
 
 function getWheat(testFileName: string): string {
 	const wheatName = testFileName.replace('.test.frg', '.wheat');
