61ebff463def2d2c4d3b94ce52a34311dc4be3e9
Siddhartha Prasad <siddhartha.a.prasad@gmail.com>
2023-12-20 17:57:27 +0530

    Closer

--- client/src/halp.ts
+++ client/src/halp.ts
@@ -1,11 +1,20 @@
 import {RacketProcess} from './racketprocess';
 import * as vscode from 'vscode';
 import * as fs from 'fs';
+import * as path from 'path';
+
+
+const WHEATSTORE = "https://sidprasad.github.io/dirtree";
 
 export async function runHalp(studentTests: string, testFileName: string): Promise<string> {
 	
-	const w = getWheat(testFileName);
-	const  w_o = await executeAgainstWheat(studentTests, w);
+	const w = await getWheat(testFileName);
+
+	if (w === "") {
+		return "HALP could not connect to the internet. Terminating run.";
+	}
+
+	const  w_o = await runTestsAgainstModel(studentTests, w);
 
 	if (w_o === "") {
 		return "Your tests were consistent with the problem specification.";
@@ -15,6 +24,7 @@ export async function runHalp(studentTests: string, testFileName: string): Promi
 		return w_o;
 	}
 
+	// Add one for test-expect.
 			
 	const regex = /Theorem Assertion (\w+) is (\w+) for (\w+) failed\./;
 	const match = w_o.match(regex);
@@ -90,99 +100,32 @@ export async function runHalp(studentTests: string, testFileName: string): Promi
 
 		w_wrapped = w_wrapped + added_pred;
 
+		const autograderTests = await getAutograderTests(testFileName);
+		const ag_output = await runTestsAgainstModel(autograderTests, w_wrapped);
+		const tName = getFailingTestName(ag_output);
 
-		// Now execute (w_wrapped) + (tests) against autograder
+		if (tName == "") {
+			return "COULD NOT GENERATE HINT. "
+		}
 
+		// Now parse the output (look for the failing test?) if none, no hint.
+		// If there is a failing test, then we can provide a hint.
 
-		// Use x, op, and y here
+		return `${tName} failed, corresponding hint was blah di blah`;
 	}
-		
-		/*
-			Theorem Assertion injective is sufficient for isDirectedTree failed. Found instance:
-			#(struct:Sat (#hash((Node . ((Node1) (Node2) (Node3))) (edges . ((Node1 Node3) (Node2 Node2) (Node3 Node1))))) ((size-variables 389) (size-clauses 606) (size-primary 20) (time-translation 167) (time-solving 16) (time-building 109)) ())\n
-		*/
-
-		// Here, we do something with the output.
-
-		// GOAL: Get a counter example, determine WHY the counter-example fails the target predicate. If we can do this,
-		// we can then even determine why an example fails.
-
-		// If the modified property is always unsat, we can ask the question : Can you think of any scenarios where <lhs pred> and <rhs pred> could both be true?
-
-
-		/*
-			## An ideal approach might be to guess at what their implementation might be.
-			
-			- Mutate the wheat so [counter example] is accepted. 
-			- Or mutate the wheat so the asserted property is true.
-			
-			Now run the autograder, and use that to generate some feedback.
-
-			## Another approach may be:
-
-			Lets get the counter example, and convert it to a Forge example. 
-			We can then run this example against a set of important *properties*.
-
-			So generate a bunch of tests:
-
-			example e1 is (is not) p1 {   				// is/isnot depends on the test. We can do some munging here to *try* and figure out which.
-				//counter example
-			}
-
-			example e2 is  (is not) p2 {
-				// counter example
-			}
-
-			Find the first example that [FAILS] --> provide the relevant feedback.
-
-		*/
-
-		/*
-		We know : The failing test's name (if it has one)
-			- Unfortunately, we don't have a nice language server that can help us parse out the test + its dependencies :(
-			- Either it is an EXAMPLE, in which case (can we help?) (or do we even want to present a hint. We may not have enough insight beyond the classic Examplar technique).
-			- Else: have a counter example an
-				- Can we use this counter example to understand anything? Could we show this to students? 
-					From last year, clearly this is *too* much info, maybe students msunderstand something?
-				- Is there any way we can determine whether the failing test fails because it is in the specified space OR otherwise?
-		
-			- Sidenote: What if it *is* in the failing part of the specification, should we ask students to comment out this test, or can we somehow skip it in the subsequent execution?
-		
-			- Concern: We don't want students to overfit to the wheat. (How do they avoid this in CS19? -- only allow examples).
-			- Can we determine if 'is necessary for' and 'is sufficient for' are both paired? We can never stop this, they could run it sequentially.
-
-			-- What if we had a bunch of properties that were all underconstraints? Then we could fail a sufficiency test  right?
-			-- What if we had a bunch of propterties that were all overconstraints? Then we could fail a necessity test  right?
-			-- Can we leverage ``test suite for {}``
-
-
-			*/
-
-		/*
-			Imagine if we only supported necessary and sufficient for HALP.
-			Parse: First print "Theorem ...... failed."
-			Alternately we could print example ABCDE failed.
-
-			If example => not sure if we can provide more insight
-			If test expect or theorem? 
-				We have a counter example. What can we do with it?
-
-				OR: Can we do Tim's example of combining multiple things?
-				SO: First try some very constrained things? OR: Most permissive wheat? OR: How do we test test cases?
-			
-
-		*/
+	// Could not generate a hint, perhaps because this was a test but not a ...
+	// At best, we can say, test is not valid.
 	return "HINT DEFAULT";
 }
 
 
-async function executeAgainstWheat (studentTests: string, wheatText: string): Promise<string> {
+async function runTestsAgainstModel (tests: string, model: string): Promise<string> {
 
 	const forgeOutput = vscode.window.createOutputChannel('HALP Output');
 	const forgeEvalDiagnostics = vscode.languages.createDiagnosticCollection('Forge Eval');
 	let racket: RacketProcess = new RacketProcess(forgeEvalDiagnostics, forgeOutput);
 
-	const toRun = combineTestsWithWheat(wheatText, studentTests);
+	const toRun = combineTestsWithWheat(model, tests);
 
 	// Write the contents of toRun to a temporary file
 	const tempFilePath = tempFile();
@@ -238,43 +181,61 @@ function tempFile(): string {
 
 
 function combineTestsWithWheat(wheatText: string, studentTests: string) : string {
-
+	// todo: What if separator doesn't exist (in that case, look for #lang forge)
 	const TEST_SEPARATOR = "//// Do not edit anything above this line ////"
 
-	// TODO: Get all the text in student Tests AFTER TEST_SEPARATOR
-	// append to wheat text and return
+	const startIndex = studentTests.indexOf(TEST_SEPARATOR) + TEST_SEPARATOR.length;
+	const studentTestsAfterSeparator = studentTests.substring(startIndex).trim();
 
-
-	return wheatText + "\n" + studentTests
+	return wheatText + "\n" + studentTestsAfterSeparator;
 }
 
-function getWheat(testFileName: string): string {
-	const wheatName = testFileName.replace('.test.frg', '.wheat');
+async function getWheat(testFileName: string): Promise<string> {
+	const wheatName = path.parse(testFileName.replace('.test.frg', '.wheat')).base;
 	
-	// Fetch wheat from [<somewhere>] , update its name appropriately.
-	// May have to fetch an array of wheats.
-
-	// Also need to makesure runsterling is off in the test file, etc.
-
-	return `#lang forge
-
-	option run_sterling off
-	sig Node {edges: set Node}
-	
-	pred isDirectedTree {
-		edges.~edges in iden -- Injective, each child has at most one parent
-		lone edges.Node - Node.edges -- At most one element that does not have a parent
-		no (^edges & iden) -- No loops
-		lone Node or Node in edges.Node + Node.edges -- Either one node or every node has either a child or a parent.
-	}`;
+	let wheatURI = `${WHEATSTORE}/${wheatName}`;
+	const response = await fetch(wheatURI);
+	if (response.ok) {
+		const wheatText = await response.text();
+		return wheatText;
+	} else {
+		// ERROR
+		return "";
+	}
 }
 
-function parseForgeOutput(w_o : string) {
 
 
-	const foundInstanceIndex = w_o.indexOf("Found instance:");
-	const leftSubstring = w_o.substring(0, foundInstanceIndex).trim();
-	const rightSubstring = w_o.substring(foundInstanceIndex + "Found instance:".length).trim();
+async function getAutograderTests(testFileName: string): Promise<string> {
+	const graderName = path.parse(testFileName.replace('.test.frg', '.grader')).base;
+	const response = await fetch(`${WHEATSTORE}/${graderName}`);
+	if (response.ok) {
+		const graderText = await response.text();
+		return graderText;
+	} else {
+		// ERROR
+		return "";
+	}
+}
 
+function getFailingTestName(o: string): string {
 
+	const assertion_regex = /Theorem Assertion (\w+) is (\w+) for (\w+) failed\./;
+	const example_regex = /Invalid example '(\w+)'; the instance specified does not satisfy the given predicate\./;
+	const test_regex = /Failed test (\w+)\./;
+
+	if (assertion_regex.test(o)) {
+		const match = o.match(assertion_regex);
+		const lhs_pred = match[1];	
+		const op = match[2];
+		const rhs_pred = match[3];
+		return "Assertion " + lhs_pred + " is " + op + " for " + rhs_pred;
+	} else if (example_regex.test(o)) {
+		const match = o.match(example_regex);
+		return match[1];
+	} else if (test_regex.test(o)) {
+		const match = o.match(example_regex);
+		return match[1];
+	} 
+	return "";
 }
