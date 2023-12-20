a9325b8bcc5f641dcd8ea9bee3a9b4d7e6101db1
Siddhartha Prasad <siddhartha.a.prasad@gmail.com>
2023-12-20 23:06:27 +0530

    IT WORKS!

--- client/src/forge-utilities.ts
+++ client/src/forge-utilities.ts
@@ -55,3 +55,107 @@ function findForgePredicates(inputText : string) : [string] {
     return predicates;
 }
 
+
+export const assertion_regex = /Theorem Assertion (\w+) is (\w+) for (\w+) failed\./;
+export const example_regex = /Invalid example '(\w+)'; the instance specified does not satisfy the given predicate\./;
+export const test_regex = /Failed test (\w+)\./;
+
+
+// s: student pred
+// i: instructor pred
+// AH, we should think of it in terms of constraining. So use ORs.
+// If the student believes that s => i 
+// then they believe that if s then i. So ease i to be { i or s }
+
+// If the student believes that i => s,
+// then they believe that if i then s. So constrict i to be { i AND s }
+
+export function adjustWheatToStudentMisunderstanding(testFileName: string, w : string, student_preds : string, w_o : string) : string {
+	const match = w_o.match(assertion_regex);
+	const lhs_pred = match[1];
+	const op = match[2];
+	const rhs_pred = match[3];
+
+	const isLhsInstructorAuthored = new RegExp(`\\b${lhs_pred}\\b`).test(w);
+	const isRhsInstructorAuthored = new RegExp(`\\b${rhs_pred}\\b`).test(w); 
+
+
+	if (isLhsInstructorAuthored && isRhsInstructorAuthored) {
+		return "No student code detected in failing assertion.";
+	}
+	else if (!isLhsInstructorAuthored && !isRhsInstructorAuthored) {
+		return "This tool can only provide further feedback if the assertion explicitly references predicates defined in the assignment specification.";
+	}
+
+	w = w + "\n" + student_preds + "\n";
+
+	// If the student believes that s => i 
+	// then they believe that if s then i. So ease i to be { i or s }
+
+	// If the student believes that i => s,
+	// then they believe that if i then s. So constrain i to be { i AND s }
+	if (isLhsInstructorAuthored) {
+
+		// i is lhs
+		// s is rhs
+		if (op == 'sufficient') {
+			// User believes lhs => rhs
+			// (ie i => s)
+			return constrainPredicate(w, lhs_pred, rhs_pred)
+
+		} else if (op == 'necessary') {
+
+			// User believes rhs => lhs
+			// ie s => i
+			return easePredicate(w, lhs_pred, rhs_pred)
+		}
+	}
+	else {
+		// i is rhs
+		// s is lhs
+		if (op == 'sufficient') {
+			// User believes lhs => rhs
+			// ie s => i
+			return easePredicate(w, rhs_pred, lhs_pred)
+
+		} else if (op == 'necessary') {
+
+			// User believes rhs => lhs
+			// ie i => s
+			return constrainPredicate(w, rhs_pred, lhs_pred)
+		}
+	}
+	return "Something went wrong!";
+}
+
+
+function constrainPredicate(w : string, i : string, s : string) : string {
+	const i_inner = i + "_inner";
+	// User believes that i => s (ie if i then s).
+	// So constrict i to be { i AND s }
+	let w_wrapped = w.replace(new RegExp("\\b" + i + "\\b", 'g'), i_inner);
+	let added_pred =
+		`
+		pred ${i} { 
+			${i_inner} 
+			${s}
+		}
+		`
+	return w_wrapped + added_pred;
+}
+
+function easePredicate(w : string, i : string, s : string) : string {
+	const i_inner = i + "_inner";
+	// User believes that s => i (ie if s then i)
+	// So ease i to be { i or s }
+	let w_wrapped = w.replace(new RegExp("\\b" + i + "\\b", 'g'), i_inner);
+	let added_pred =
+		`
+		pred ${i} { 
+			${i_inner} or ${s}
+		}
+		`
+	return w_wrapped + added_pred;
+}
+
+
--- client/src/halp.ts
+++ client/src/halp.ts
@@ -2,22 +2,12 @@ import {RacketProcess} from './racketprocess';
 import * as vscode from 'vscode';
 import * as fs from 'fs';
 import * as path from 'path';
-import { getPredicatesOnly } from './forge-utilities'; 
+import { assertion_regex, example_regex, test_regex, adjustWheatToStudentMisunderstanding, getPredicatesOnly } from './forge-utilities'; 
 /*
 	Potential issues : Name clash between student files and grader files.
 */
 
 const WHEATSTORE = "https://sidprasad.github.io/dirtree";
-
-
-const assertion_regex = /Theorem Assertion (\w+) is (\w+) for (\w+) failed\./;
-const example_regex = /Invalid example '(\w+)'; the instance specified does not satisfy the given predicate\./;
-const test_regex = /Failed test (\w+)\./;
-
-
-const SATTESTNAME = "satisfiability"
-
-
 
 export async function runHalp(studentTests: string, testFileName: string): Promise<string> {
 	const w = await getWheat(testFileName);
@@ -32,9 +22,6 @@ export async function runHalp(studentTests: string, testFileName: string): Promi
 
 	const testName = getFailingTestName(w_o);
 
-
-
-
 	// TODO: This default text  is what is shown where we are in the modelling space
 	// (or more correctly, NOT an example and NOT in the autograder space)
 	// It's not that they are right, we do not know if they are specifically wrong.
@@ -46,18 +33,6 @@ export async function runHalp(studentTests: string, testFileName: string): Promi
 	if (example_regex.test(w_o)) {
 		return w_o;
 	}
-
-
-
-
-	///// BUG !!!! /////
-	// The following code is broken. Need to fix. The issue here is that we need *all* the predicates the student wrote.
-	// but not their tests. So we need to parse the student's code and extract all the predicates. (And potentially any instances.)
-	// then we need to add this to the wheat!
-
-
-	// We can start with only predicates (A language server would be nice here!!)
-
 	if (assertion_regex.test(w_o)) {
 		const student_preds = getPredicatesOnly(studentTests); 
 
@@ -196,7 +171,7 @@ function getFailingTestName(o: string): string {
 		const match = o.match(example_regex);
 		return match[1];
 	} else if (test_regex.test(o)) {
-		const match = o.match(example_regex);
+		const match = o.match(test_regex);
 		return match[1];
 	} 
 	return "";
@@ -205,86 +180,12 @@ function getFailingTestName(o: string): string {
 // w : wheat
 // w_o : wheat output
 async function tryGetHintFromAssertion(testFileName: string, w : string, student_preds : string, w_o : string) : Promise<string> {
-	const match = w_o.match(assertion_regex);
 
-	if (match) {
-		const lhs_pred = match[1];
-		const op = match[2];
-		const rhs_pred = match[3];
-
-		const lhs_pred_inner = lhs_pred + "_inner";
-		const rhs_pred_inner = rhs_pred + "_inner";
-
-		const isLhsPredContained = new RegExp(`\\b${lhs_pred}\\b`).test(w);
-		const isRhsPredContained = new RegExp(`\\b${rhs_pred}\\b`).test(w); 
-
-
-		if (isLhsPredContained && isRhsPredContained) {
-			return "No student code detected in failing assertion.";
-		}
-		else if (!isLhsPredContained && !isRhsPredContained) {
-			return "This tool can only provide further feedback if the assertion explicitly references predicates defined in the assignment specification.";
-		}
-
-
-		w = w + "\n" + student_preds + "\n";
-		var w_wrapped = "";
-		var added_pred = "";		
-
-		if (isLhsPredContained) {
-			w_wrapped = w.replace(new RegExp("\\b" + lhs_pred + "\\b", 'g'), lhs_pred_inner);
-
-			added_pred = (op == 'sufficient') ? 
-			`
-			pred ${lhs_pred} { 
-				${lhs_pred_inner}
-				${lhs_pred_inner} implies ${rhs_pred}
-			}
-			` : 
-			`
-			pred ${lhs_pred} { 
-				${lhs_pred_inner}
-				${rhs_pred} implies ${lhs_pred_inner}
-			}
-			`;
-
-			added_pred += `
-			test expect {
-				${SATTESTNAME} : {${lhs_pred_inner} and ${rhs_pred}} is sat
-			}
-			`
-		}
-		else {	
-			w_wrapped = w.replace(new RegExp("\\b" + rhs_pred + "\\b", 'g'), rhs_pred_inner);
-			added_pred = (op == 'sufficient') ? 
-			`
-			pred ${rhs_pred} { 
-				${rhs_pred_inner}
-				${lhs_pred} implies ${rhs_pred_inner}
-			}
-			` : 
-			`
-			pred ${rhs_pred} { 
-				${rhs_pred_inner} 
-				${rhs_pred_inner} implies ${lhs_pred}
-			}
-			`;
-			added_pred += `test expect {
-				${SATTESTNAME} : {${lhs_pred} and ${rhs_pred_inner}} is sat
-			}
-			`;
-		}
-		w_wrapped = w_wrapped + added_pred;
-
-		const autograderTests = await getAutograderTests(testFileName);
-		const ag_output = await runTestsAgainstModel(autograderTests, w_wrapped);
-		const tName = getFailingTestName(ag_output);
-
-		var hint_text = getHintMap(testFileName)[tName] || "";
-		if (tName == SATTESTNAME) {
-			return "Can both predicates in this assertion hold simultaneously?"
-		}
-		return hint_text;
-	}
-	return "";
+	let w_wrapped = adjustWheatToStudentMisunderstanding(testFileName, w, student_preds, w_o);
+	const autograderTests = await getAutograderTests(testFileName);
+	const ag_output = await runTestsAgainstModel(autograderTests, w_wrapped);
+	const tName = getFailingTestName(ag_output);
+	const hint_map = await getHintMap(testFileName)
+	const hint_text = hint_map[tName] || "";
+	return hint_text;
 }
