3453741033d2636bc1ad076c3e133cf61b2f48b2
Siddhartha Prasad <siddhartha.a.prasad@gmail.com>
2024-02-15 17:56:43 -0500

    Update

--- .github/scripts/version-check.js
+++ .github/scripts/version-check.js
@@ -2,17 +2,20 @@ const { execSync } = require('child_process');
 const { readFileSync } = require('fs');
 const { join } = require('path');
 
-const getPackageVersion = (path) => {
-  const packageJsonPath = join(path, 'package.json');
-  const packageJson = JSON.parse(readFileSync(packageJsonPath));
+const getPackageVersion = (branch) => {
+
+
+  const packageJsonPath = 'package.json';
+  const package_contents = execSync(`git show ${branch}:${packageJsonPath}`).toString().trim();
+  const packageJson = JSON.parse(package_contents);
+  //console.log(`Version: ${packageJson.version}`);
   return packageJson.version;
 };
 
-const mainBranchVersion = getPackageVersion('.');
-const prBranchVersion = execSync('git merge-base HEAD main').toString().trim();
-const baseVersion = getPackageVersion(prBranchVersion);
+const currBranchVersion = getPackageVersion('HEAD');
+const mainVersion = getPackageVersion('main');
 
-if (mainBranchVersion === baseVersion) {
+if (currBranchVersion == mainVersion) {
   console.error('Version number must be incremented.');
   process.exit(1); // Fails the workflow if versions are the same
 } else {
